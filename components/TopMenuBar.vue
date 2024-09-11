3dedb77bc59fac55b9871f5e7b1e8d449e823262
Slavina Stefanova <sstefanova@wikimedia.org>
2024-09-11 22:40:52 +0200

    make it work on screens of different sizes

--- components/TopMenuBar.vue
+++ components/TopMenuBar.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="navbar bg-base-100 shadow-lg">
-    <div class="flex-1">
-      <NuxtLink to="/" class="btn btn-ghost normal-case text-2xl">Toolhunt</NuxtLink>
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost normal-case text-xl">Toolhunt</NuxtLink>
     </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1 items-center">
+    <div class="navbar-end">
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li><NuxtLink to="/leaderboard">Leaderboard</NuxtLink></li>
+          <li><NuxtLink to="/profile">Profile</NuxtLink></li>
+          <li>
+            <a v-if="authState.user" @click="logout" class="text-primary hover:text-primary-focus">Logout</a>
+            <a v-else @click="login" class="text-primary hover:text-primary-focus">Login</a>
+          </li>
+        </ul>
+      </div>
+      <ul class="menu menu-horizontal px-1 hidden lg:flex items-center">
         <li><NuxtLink to="/leaderboard" class="text-lg px-4 py-2">Leaderboard</NuxtLink></li>
         <li><NuxtLink to="/profile" class="text-lg px-4 py-2">Profile</NuxtLink></li>
         <li class="ml-2">
-          <a v-if="authState.user" @click="logout" class="btn btn-outline btn-primary normal-case px-4 py-2">Logout</a>
-          <a v-else @click="login" class="btn btn-primary normal-case px-4 py-2">Login</a>
+          <a v-if="authState.user" @click="logout" class="btn btn-outline btn-primary normal-case">Logout</a>
+          <a v-else @click="login" class="btn btn-primary normal-case">Login</a>
         </li>
       </ul>
     </div>
