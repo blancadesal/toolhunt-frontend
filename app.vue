9ada6afbaaac9534bcf7c7cf46cebad5bd736b16
Slavina Stefanova <sstefanova@wikimedia.org>
2024-09-16 13:18:55 +0200

    footer UI

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div data-theme="emerald">
     <TopMenuBar />
     <NuxtPage />
-    <footer class="footer footer-center bg-primary text-secondary-content p-4">
+    <footer class="footer footer-center bg-primary text-primary-content p-2">
       <aside>
         <p>Copyright © {{ new Date().getFullYear() }} - All rights reserved by Toolhunters Inc.</p>
       </aside>
